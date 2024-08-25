488f4b06c96e057d6bd9b525a75e194c7bd1f671
tiago <tiago.dscouto@hotmail.com>
2024-08-25 18:02:44 -0300

    finish project

--- scripts/cadastro_post.js
+++ scripts/cadastro_post.js
@@ -1,23 +1,217 @@
 function cadastrarPost(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    
+  const post = {
+    id: Date.now(),
+    nome: document.getElementById('nome').value,
+    email: document.getElementById('email').value,
+    dataCriacao: new Date().toLocaleDateString(),
+    senha: document.getElementById('senha').value,
+    descricao: document.getElementById('descricao').value,
+    foto: document.getElementById('foto').value,
+  };
 
-    const post = {
-        id: Date.now(),
-        titulo: document.getElementById('titulo').value,
-        descricao: document.getElementById('descricao').value,
-        dataCriacao: new Date().toLocaleDateString(),  
-        categoria: document.getElementById('categoria').value,
-        foto: document.getElementById('foto').value
-    };
+  let posts = JSON.parse(localStorage.getItem('posts')) || [];
+  posts.push(post);
+  localStorage.setItem('posts', JSON.stringify(posts));
 
-    let posts = JSON.parse(localStorage.getItem('posts')) || [];
-    posts.push(post);
-    localStorage.setItem('posts', JSON.stringify(posts));
-
-    alert('Post salvo com sucesso!');
-    document.getElementById('postForm').reset();
+  alert('Post salvo com sucesso!');
+  document.getElementById('postForm').reset();
 }
 
 document.getElementById('postForm').addEventListener('submit', cadastrarPost);
+
+// function cadastrarUsuario(event) {
+//   event.preventDefault();
+
+//   let confirmaSenha = document.getElementById('confirma-senha').value;
+
+//   const usuario = {
+//     id: Date.now(),
+//     nome: document.getElementById('nome').value,
+//     email: document.getElementById('email').value,
+//     dataCriacao: new Date().toLocaleDateString(),
+//     senha: document.getElementById('senha').value,
+//     descricao: document.getElementById('descricao').value,
+//     foto: document.getElementById('foto').value,
+//   };
+
+//   if (usuario.foto === '') {
+//     document.getElementById('foto').style.borderWidth = '2px';
+//     document.getElementById('foto').style.borderColor = 'red';
+//     document.getElementById('error-foto').innerText =
+//       'Foto do usuario é obrigatória';
+//     document.getElementById('error-foto').style.color = 'red';
+//     console.log('precisa de foto');
+//   } else {
+//     document.getElementById('foto').style.border = '';
+//     document.getElementById('error-foto').innerText = '';
+//     document.getElementById('error-foto').style.color = '';
+//   }
+
+//   if (usuario.nome === '') {
+//     // alert("Nome é obrigatório")
+//     document.getElementById('nome').style.borderColor = 'red';
+//     document.getElementById('nome').style.borderWidth = '2px';
+//     document.getElementById('error-nome').innerText = 'Nome é obrigatório';
+//     document.getElementById('error-nome').style.color = 'red';
+//   } else {
+//     document.getElementById('nome').style.borderColor = '';
+//     document.getElementById('error-nome').innerText = '';
+//     document.getElementById('error-nome').style.color = '';
+//   }
+
+//   if (usuario.email === '') {
+//     // alert("Nome é obrigatório")
+//     document.getElementById('email').style.borderColor = 'red';
+//     document.getElementById('email').style.borderWidth = '2px';
+//     document.getElementById('error-email').innerText = 'Email é obrigatório';
+//     document.getElementById('error-email').style.color = 'red';
+//   } else {
+//     document.getElementById('email').style.borderColor = '';
+//     document.getElementById('error-email').innerText = '';
+//     document.getElementById('error-email').style.color = '';
+//   }
+
+//   if (usuario.senha === '') {
+//     // alert("Nome é obrigatório")
+//     document.getElementById('senha').style.borderColor = 'red';
+//     document.getElementById('senha').style.borderWidth = '2px';
+//     document.getElementById('error-senha').innerText = 'Senha é obrigatório';
+//     document.getElementById('error-senha').style.color = 'red';
+//   } else {
+//     document.getElementById('senha').style.borderColor = '';
+//     document.getElementById('error-senha').innerText = '';
+//     document.getElementById('error-senha').style.color = '';
+//   }
+
+//   if (confirmaSenha === '') {
+//     // alert("Nome é obrigatório")
+//     document.getElementById('confirma-senha').style.borderColor = 'red';
+//     document.getElementById('confirma-senha').style.borderWidth = '2px';
+//     document.getElementById('error-confirma-senha').innerText =
+//       'Confirma a senha é obrigatório';
+//     document.getElementById('error-confirma-senha').style.color = 'red';
+//   } else {
+//     document.getElementById('confirma-senha').style.borderColor = '';
+//     document.getElementById('error-confirma-senha').innerText = '';
+//     document.getElementById('error-confirma-senha').style.color = '';
+//   }
+
+//   if (usuario.descricao === '') {
+//     // alert("Nome é obrigatório")
+//     document.getElementById('descricao').style.borderColor = 'red';
+//     document.getElementById('descricao').style.borderWidth = '2px';
+//     document.getElementById('error-descricao').innerText =
+//       'Biografia do usuario  é obrigatório';
+//     document.getElementById('error-descricao').style.color = 'red';
+//   } else {
+//     document.getElementById('descricao').style.borderColor = '';
+//     document.getElementById('error-descricao').innerText = '';
+//     document.getElementById('error-descricao').style.color = '';
+//   }
+
+//   if (
+//     usuario.nome &&
+//     usuario.foto &&
+//     usuario.senha &&
+//     confirmaSenha &&
+//     usuario.descricao &&
+//     usuario.email
+//   ) {
+//     if (usuario.senha !== confirmaSenha) {
+//       alert('Senha errada, digite novamente');
+//     } else {
+//       let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+//       usuarios.push(usuario);
+//       localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+//       alert('Usuarios salvo com sucesso!');
+//       document.getElementById('postForm').reset();
+//       document.getElementById('foto-usuario').setAttribute('src', '');
+//     }
+//   }
+// }
+
+// document.getElementById('foto').addEventListener('blur', function (event) {
+//   // event.target.value
+//   document.getElementById('foto-usuario').setAttribute('src', this.value);
+// });
+
+// document
+//   .getElementById('postForm')
+//   .addEventListener('submit', cadastrarUsuario);
+
+function setError(element, message) {
+  element.style.borderColor = 'red';
+  element.style.borderWidth = '2px';
+  const errorElement = document.getElementById(`error-${element.id}`);
+  errorElement.innerText = message;
+  errorElement.style.color = 'red';
+}
+
+function clearError(element) {
+  element.style.borderColor = '';
+  const errorElement = document.getElementById(`error-${element.id}`);
+  errorElement.innerText = '';
+  errorElement.style.color = '';
+}
+
+function validateField(field) {
+  if (field.value === '') {
+    setError(field, `${field.name} é obrigatório`);
+    return false;
+  } else {
+    clearError(field);
+    return true;
+  }
+}
+
+function cadastrarUsuario(event) {
+  event.preventDefault();
+
+  const fields = [
+    'nome',
+    'email',
+    'senha',
+    'confirma-senha',
+    'descricao',
+    'foto',
+  ].map(id => document.getElementById(id));
+  const allFieldsValid = fields.every(validateField);
+
+  if (allFieldsValid) {
+    const [nome, email, senha, confirmaSenha, descricao, foto] = fields;
+
+    if (senha.value !== confirmaSenha.value) {
+      alert('As senhas não correspondem');
+      setError(confirmaSenha, 'As senhas não correspondem');
+    } else {
+      const usuario = {
+        id: Date.now(),
+        nome: nome.value,
+        email: email.value,
+        dataCriacao: new Date().toLocaleDateString(),
+        senha: senha.value,
+        descricao: descricao.value,
+        foto: foto.value,
+      };
+
+      const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+      usuarios.push(usuario);
+      localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+      alert('Usuário salvo com sucesso!');
+      document.getElementById('postForm').reset();
+      document.getElementById('foto-usuario').setAttribute('src', '');
+    }
+  }
+}
+
+document.getElementById('foto').addEventListener('blur', function () {
+  document.getElementById('foto-usuario').setAttribute('src', this.value);
+});
+
+document
+  .getElementById('postForm')
+  .addEventListener('submit', cadastrarUsuario);
--- scripts/listagem_post_admin.js
+++ scripts/listagem_post_admin.js
@@ -1,62 +1,137 @@
 function loadPosts() {
-    const postsTableBody = document.querySelector('#postsTable tbody');
-    let posts = JSON.parse(localStorage.getItem('posts')) || [];
+  const postsTableBody = document.querySelector('#postsTable tbody');
+  let posts = JSON.parse(localStorage.getItem('posts')) || [];
 
-    postsTableBody.innerHTML = ''; // Limpa a tabela
+  postsTableBody.innerHTML = ''; // Limpa a tabela
 
-    if (posts.length === 0) {
-        const emptyRow = document.createElement('tr');
-        const emptyCell = document.createElement('td');
-        emptyCell.setAttribute('colspan', '6');
-        emptyCell.textContent = 'Nenhum post encontrado.';
-        emptyRow.appendChild(emptyCell);
-        postsTableBody.appendChild(emptyRow);
-    } else {
-        posts.forEach((post, index) => {
-            const row = document.createElement('tr');
+  if (posts.length === 0) {
+    const emptyRow = document.createElement('tr');
+    const emptyCell = document.createElement('td');
+    emptyCell.setAttribute('colspan', '6');
+    emptyCell.textContent = 'Nenhum post encontrado.';
+    emptyRow.appendChild(emptyCell);
+    postsTableBody.appendChild(emptyRow);
+  } else {
+    posts.forEach((post, index) => {
+      const row = document.createElement('tr');
 
-            const tituloCell = document.createElement('td');
-            tituloCell.textContent = post.titulo;
-            row.appendChild(tituloCell);
+      const tituloCell = document.createElement('td');
+      tituloCell.textContent = post.titulo;
+      row.appendChild(tituloCell);
 
-            const descricaoCell = document.createElement('td');
-            descricaoCell.textContent = post.descricao;
-            row.appendChild(descricaoCell);
+      const descricaoCell = document.createElement('td');
+      descricaoCell.textContent = post.descricao;
+      row.appendChild(descricaoCell);
 
-            const categoriaCell = document.createElement('td');
-            categoriaCell.textContent = post.categoria;
-            row.appendChild(categoriaCell);
+      const categoriaCell = document.createElement('td');
+      categoriaCell.textContent = post.categoria;
+      row.appendChild(categoriaCell);
 
-            const dataCriacaoCell = document.createElement('td');
-            dataCriacaoCell.textContent = new Date(post.dataCriacao).toLocaleDateString();
-            row.appendChild(dataCriacaoCell);
+      const dataCriacaoCell = document.createElement('td');
+      dataCriacaoCell.textContent = new Date(
+        post.dataCriacao
+      ).toLocaleDateString();
+      row.appendChild(dataCriacaoCell);
 
-            const fotoCell = document.createElement('td');
-            const fotoImg = document.createElement('img');
-            fotoImg.setAttribute('src', post.foto);
-            fotoImg.setAttribute('alt', post.titulo);
-            fotoImg.style.maxWidth = '100px';
-            fotoCell.appendChild(fotoImg);
-            row.appendChild(fotoCell);
+      const fotoCell = document.createElement('td');
+      const fotoImg = document.createElement('img');
+      fotoImg.setAttribute('src', post.foto);
+      fotoImg.setAttribute('alt', post.titulo);
+      fotoImg.style.maxWidth = '100px';
+      fotoCell.appendChild(fotoImg);
+      row.appendChild(fotoCell);
 
-            const actionCell = document.createElement('td');
-            const deleteButton = document.createElement('button');
-            deleteButton.classList.add('delete-btn');
-            deleteButton.textContent = 'Deletar';
-            deleteButton.onclick = () => deletePost(index);
-            actionCell.appendChild(deleteButton);
-            row.appendChild(actionCell);
+      const actionCell = document.createElement('td');
+      const deleteButton = document.createElement('button');
+      deleteButton.classList.add('delete-btn');
+      deleteButton.textContent = 'Deletar';
+      deleteButton.onclick = () => deletePost(index);
+      actionCell.appendChild(deleteButton);
+      row.appendChild(actionCell);
 
-            postsTableBody.appendChild(row);
-        });
-    }
+      postsTableBody.appendChild(row);
+    });
+  }
 }
 
 function deletePost(index) {
-    let posts = JSON.parse(localStorage.getItem('posts')) || [];
-    posts = posts.filter((post, i) => i !== index);
-    localStorage.setItem('posts', JSON.stringify(posts));
-    loadPosts();
+  let posts = JSON.parse(localStorage.getItem('posts')) || [];
+  posts = posts.filter((post, i) => i !== index);
+  localStorage.setItem('posts', JSON.stringify(posts));
+  loadPosts();
+}
+
+document.addEventListener('DOMContentLoaded', loadPosts);
+
+function createCard({ id, titulo, dataCriacao, descricao, categoria, foto }) {
+  const classes = [
+    'post',
+    'img-wrapper',
+    'post-info',
+    'poster-read',
+    'poster-foot',
+  ];
+  const [postDiv, imgWrapperDiv, postInfo, postHeadDiv, postFootDiv] =
+    createDom('div', 'div', 'div', 'div', 'div').map((div, index) => {
+      div.setAttribute('class', classes[index]);
+      return div;
+    });
+
+  const [headerSpan, postTitle, dateSpan, readSpan, img] = createDom(
+    'span',
+    'h2',
+    'span',
+    'span',
+    'img'
+  );
+
+  imgWrapperDiv.appendChild(img);
+
+  postHeadDiv.append(headerSpan, postTitle);
+  postFootDiv.append(dateSpan, readSpan);
+  postInfo.append(postHeadDiv, postFootDiv);
+
+  postDiv.append(imgWrapperDiv, postInfo);
+
+  const readingTime = (descricao.length * 60) / 200;
+  let msg =
+    readingTime < 60
+      ? `${Math.ceil(readingTime)} segundos`
+      : `${readingTime / 60} minutos`;
+
+  //const msg = `${readingTime / 60}`
+
+  img.src = foto;
+  postTitle.innerText = titulo;
+  headerSpan.innerText = categoria;
+  dateSpan.innerText = dataCriacao;
+  readSpan.innerText = `${msg} de leitura`;
+
+  addEvent(postDiv, id);
+
+  return postDiv;
+
+  /*
+        const divs = createDom(['div', 'div', 'div', 'div'])
+
+        const classes = ['post', 'img-wrappe', 'poster-foot']
+        
+        divs.forEach( (div, index) => {
+            div.setAttribute('class', classes[ index ])
+        })
+    */
+}
+
+function loadPosts() {
+  const posts = document.getElementById('posts-view');
+
+  const storageObj = storage.get();
+
+  storageObj.posts.forEach(postData => {
+    const card = createCard(postData);
+
+    posts.appendChild(card);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', loadPosts);
